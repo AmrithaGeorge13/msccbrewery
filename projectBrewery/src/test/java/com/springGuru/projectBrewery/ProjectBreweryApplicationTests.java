74fd9edc2857dd1a393704ab36e80df61e730dc7
AmrithaGeorge13 <44130808+AmrithaGeorge13@users.noreply.github.com>
2021-02-10 09:11:02 +0530

    project set up
    project set up for mscc brewery

---
+++ projectBrewery/src/test/java/com/springGuru/projectBrewery/ProjectBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springGuru.projectBrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
